852e53740b08269b8685e57703fe86462dbc2d7e
DerekRin <derekrin98@gmail.com>
2022-04-27 16:48:37 -0500

    connect to jawsdb

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,23 @@ const routes = require("./routes/routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));

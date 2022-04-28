e343ee60d5c6d6831115901112d3944561dd2087
DerekRin <derekrin98@gmail.com>
2022-04-27 21:26:16 -0500

    next 2

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
-const Sequelize = require("sequelize");
 const path = require("path");
 const express = require("express");
 const routes = require("./routes/routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3001,
-    }
-  );
-}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
